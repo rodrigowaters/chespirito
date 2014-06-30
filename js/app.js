f3ec5ac1159cf898ecfece0b5ea635124674ba3f
Rodrigo Waters <rdrgwtrs@gmail.com>
2014-06-29 23:47:47 -0300

    fix list

--- js/app.js
+++ js/app.js
@@ -2,41 +2,41 @@ $(document).ready(function() {
     var $personagens = {};
     var $page_main = $('main#main');
     var $page_view = $('main#view');
-    $.getJSON("personagens.json", function(data) {
-        var item = [];
-        $.each(data, function(imagem, personagem) {
-            $personagens[imagem] = personagem;
-            var $template = '<div id_personagem="' + imagem + '" class="item item-avatar item-button-right"><img src="images/personagens/' + imagem + '.png"><h2>' + personagem.name + '</h2><button class="button button-positive"><i class="icon ion-arrow-right-a"></i></button></div>';
-            item.push($template);
+    $.getJSON("personagens.json", function($data) {
+        var $item = [];
+        $.each($data, function($imagem, $personagem) {
+            $personagens[$imagem] = $personagem;
+            var $template = '<li id_personagem="' + $imagem + '" class="item item-avatar item-button-right"><img src="images/personagens/' + $imagem + '.png"><h2>' + $personagem.name + '</h2><button class="button button-positive"><i class="icon ion-arrow-right-a"></i></button></li>';
+            $item.push($template);
         });
         $("<ul/>", {
             "class": "list",
-            html: item.join("")
-        }).appendTo("main#main article").find("div.item").click(function() {
+            html: $item.join("")
+        }).appendTo("main#main article").find("li.item").click(function() {
             var $nodeIndex = $(this).attr('id_personagem');
             var $personagem = $personagens[$nodeIndex];
             var $audio = 1;
             $page_view.find('header div.title').html($personagem.name);
             $page_view.find('img').prop('src', 'images/personagens/' + $nodeIndex + '.png');
-            var $content_to_append = '<div class="item item-divider">Descrição</div><div class="item">' + $personagem.description + '</div>';
-            $content_to_append += '<div class="item item-divider">Ator</div><div class="item"">' + $personagem.actor + '</div>';
+            var $content_to_append = '<li class="item item-divider">Descrição</li><li class="item">' + $personagem.description + '</li>';
+            $content_to_append += '<li class="item item-divider">Ator</li><li class="item"">' + $personagem.actor + '</li>';
             if ($personagem.phrases.length > 0) {
-                $content_to_append += '<div class="item item-divider">Frases</div>';
+                $content_to_append += '<li class="item item-divider">Frases</li>';
                 for (var key in $personagem.phrases) {
                     if ($.isPlainObject($personagem.phrases[key])) {
-                        $content_to_append += '<div class="item item-button-right">' + $personagem.phrases[key].text + '<button class="button button-positive"><i class="icon ion-ios7-play"><audio><source src="sounds/' + $nodeIndex + '_' + $audio++ + '.wav" type="audio/wav"></audio></i></button></div>';
+                        $content_to_append += '<li class="item item-button-right">' + $personagem.phrases[key].text + '<button class="button button-positive"><i class="icon ion-ios7-play"><audio><source src="sounds/' + $nodeIndex + '_' + $audio++ + '.wav" type="audio/wav"></audio></i></button></li>';
                     } else {
-                        $content_to_append += '<div class="item" href="#">' + $personagem.phrases[key] + '</div>';
+                        $content_to_append += '<li class="item">' + $personagem.phrases[key] + '</li>';
                     }
                 }
             }
             if ($personagem.nicknames.length > 0) {
-                $content_to_append += '<div class="item item-divider">Apelidos</div>';
+                $content_to_append += '<li class="item item-divider">Apelidos</li>';
                 for (var key in $personagem.nicknames) {
-                    $content_to_append += '<div class="item">' + $personagem.nicknames[key] + '</div>';
+                    $content_to_append += '<li class="item">' + $personagem.nicknames[key] + '</li>';
                 }
             }
-            $page_view.find('div.list').html($content_to_append).find('div.item button').click(function() {
+            $page_view.find('li.list').html($content_to_append).find('li.item button').click(function() {
                 var audio = $(this).find('audio')[0];
                 audio.play();
             });
