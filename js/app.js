673bb22f063633899efac50fe740dcff9129fa8a
Rodrigo Waters <rdrgwtrs@gmail.com>
2014-06-29 22:56:48 -0300

    Release version 1.0
    new layout

--- js/app.js
+++ js/app.js
@@ -1,52 +1,51 @@
 $(document).ready(function() {
-    $.mobile.loading("show");
-    var page_detals = "#detals";
-    var personagens = {};
+    var $personagens = {};
+    var $page_main = $('main#main');
+    var $page_view = $('main#view');
     $.getJSON("personagens.json", function(data) {
         var item = [];
         $.each(data, function(imagem, personagem) {
-            personagens[imagem] = personagem;
-            item.push('<li id_personagem="' + imagem + '"><a href="#"><img src="images/personagens/' + imagem + '.png" class="ui-li-thumb"><h2>' + personagem.name + '</h2><p>' + personagem.description + '</p></a></li>');
+            $personagens[imagem] = personagem;
+            var $template = '<a id_personagem="' + imagem + '" class="item item-avatar item-button-right" href="#"><img src="images/personagens/' + imagem + '.png"><h2>' + personagem.name + '</h2><button class="button button-positive"><i class="icon ion-arrow-right-a"></i></button></a>';
+            item.push($template);
         });
         $("<ul/>", {
-            "data-role": "listview",
-            "data-inset": "true",
-            "class": "my-new-list",
+            "class": "list",
             html: item.join("")
-        }).appendTo("#main-page .ui-content").find("li").click(function() {
-            $.mobile.loading('show');
-            var nodeIndex = $(this).attr('id_personagem');
-            var personagem = personagens[nodeIndex];
-            $(page_detals).find('div[data-role="header"] h1').text(personagem.name);
-            var detals = $(page_detals).find('div[role="main"]');
-            detals.find('img').prop('src', 'images/personagens/' + nodeIndex + '.png');
-            var appendDetals = '<li data-role="list-divider">Descrição</li><li>' + personagem.description + '</li>';
-            appendDetals += '<li data-role="list-divider">Ator</li><li>' + personagem.actor + '</li>';
-            var audio = 1;
-            if (personagem.phrases.length > 0) {
-                appendDetals += '<li data-role="list-divider">Frases</li>';
-                for (var key in personagem.phrases) {
-                    if ($.isPlainObject(personagem.phrases[key])) {
-                        appendDetals += '<li><a href="#">' + personagem.phrases[key].text + '<audio><source src="sounds/' + nodeIndex + '_' + audio++ + '.wav" type="audio/wav"></audio></a></li>';
+        }).appendTo("main#main article").find("a").click(function() {
+            var $nodeIndex = $(this).attr('id_personagem');
+            var $personagem = $personagens[$nodeIndex];
+            var $audio = 1;
+            $page_view.find('header div.title').html($personagem.name);
+            $page_view.find('img').prop('src', 'images/personagens/' + $nodeIndex + '.png');
+            var $content_to_append = '<div class="item item-divider">Descrição</div><div class="item">' + $personagem.description + '</div>';
+            $content_to_append += '<div class="item item-divider">Ator</div><div class="item"">' + $personagem.actor + '</div>';
+            if ($personagem.phrases.length > 0) {
+                $content_to_append += '<div class="item item-divider">Frases</div>';
+                for (var key in $personagem.phrases) {
+                    if ($.isPlainObject($personagem.phrases[key])) {
+                        $content_to_append += '<div class="item item-button-right">' + $personagem.phrases[key].text + '<button class="button button-positive"><i class="icon ion-ios7-play"><audio><source src="sounds/' + $nodeIndex + '_' + $audio++ + '.wav" type="audio/wav"></audio></i></button></div>';
                     } else {
-                        appendDetals += '<li>' + personagem.phrases[key] + '</li>';
+                        $content_to_append += '<div class="item" href="#">' + $personagem.phrases[key] + '</div>';
                     }
                 }
             }
-            if (personagem.nicknames.length > 0) {
-                appendDetals += '<li data-role="list-divider">Apelidos</li>';
-                for (var key in personagem.nicknames) {
-                    appendDetals += '<li>' + personagem.nicknames[key] + '</li>';
+            if ($personagem.nicknames.length > 0) {
+                $content_to_append += '<div class="item item-divider">Apelidos</div>';
+                for (var key in $personagem.nicknames) {
+                    $content_to_append += '<div class="item">' + $personagem.nicknames[key] + '</div>';
                 }
             }
-            detals.find('ul').html(appendDetals).listview().listview('refresh').find('a').click(function() {
+            $page_view.find('div.list').html($content_to_append).find('div.item button').click(function() {
                 var audio = $(this).find('audio')[0];
                 audio.play();
             });
-            $.mobile.changePage(page_detals, {transition: "pop", changeHash: false});
-            $.mobile.loading("hide");
+            $page_main.removeClass('left_to_main').addClass('main_to_left');
+            $page_view.removeClass('main_to_right').addClass('right_to_main');
         });
-        $("#main-page .ui-content ul").listview();
-        $.mobile.loading("hide");
+    });
+    $page_view.find('header button').click(function(){
+        $page_main.removeClass('main_to_left').addClass('left_to_main');
+        $page_view.removeClass('right_to_main').addClass('main_to_right');
     });
 });
